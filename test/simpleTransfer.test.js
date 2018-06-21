fa2f178b372b62dfd40ab30193ece8b539214d0d
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-21 21:05:11 +0700

    strest test

--- test/simpleTransfer.test.js
+++ test/simpleTransfer.test.js
@@ -3,15 +3,17 @@ const { TransactionService } = require('../src/transaction.service');
 const { Transaction, Account } = require('../src/models');
 
 describe('Test simple transfer function', async () => {
-    let idAccount1, idAccount2;
+    let idAccount1, idAccount2, idAccount3;
 
     beforeEach('Create accounts', async () => {
         const account1 = new Account({ name: 'teo', email: 'teo@gmail.com', balance: 200 });
         const account2 = new Account({ name: 'ti', email: 'ti@gmail.com', balance: 500 });
+        const account3 = new Account({ name: 'ti', email: 'ti@gmail.com', balance: 500 }); //do not save this account
         await account1.save();
         await account2.save();
         idAccount1 = account1._id;
         idAccount2 = account2._id;
+        idAccount3 = account3._id;
     });
 
     it('Can transfer money', async () => {
@@ -34,6 +36,33 @@ describe('Test simple transfer function', async () => {
     it('Cannot transfer negative amount', async () => {
         const errorMessage = await TransactionService.transfer(idAccount1, idAccount2, -300).catch(error => error.message);
         equal(errorMessage, 'INVALID_AMOUNT');
+        const account1 = await Account.findById(idAccount1);
+        const account2 = await Account.findById(idAccount2);
+        equal(account1.balance, 200);
+        equal(account2.balance, 500);
+    });
+
+    it('Cannot transfer from unsaved account', async () => {
+        const errorMessage = await TransactionService.transfer(idAccount3, idAccount2, 300).catch(error => error.message);
+        equal(errorMessage, 'CANNOT_FIND_SOURCE_ACCOUNT');
+        const account1 = await Account.findById(idAccount1);
+        const account2 = await Account.findById(idAccount2);
+        equal(account1.balance, 200);
+        equal(account2.balance, 500);
+    });
+
+    it('Cannot transfer from unsaved account', async () => {
+        const errorMessage = await TransactionService.transfer(idAccount3, idAccount2, 300).catch(error => error.message);
+        equal(errorMessage, 'CANNOT_FIND_SOURCE_ACCOUNT');
+        const account1 = await Account.findById(idAccount1);
+        const account2 = await Account.findById(idAccount2);
+        equal(account1.balance, 200);
+        equal(account2.balance, 500);
+    });
+
+    it('Cannot transfer to unsaved account', async () => {
+        const errorMessage = await TransactionService.transfer(idAccount1, idAccount3, 300).catch(error => error.message);
+        equal(errorMessage, 'CANNOT_FIND_DESTINATION_ACCOUNT');
         const account1 = await Account.findById(idAccount1);
         const account2 = await Account.findById(idAccount2);
         equal(account1.balance, 200);
